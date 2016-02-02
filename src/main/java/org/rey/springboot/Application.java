723e8bbf9b24692bfc2118e9ddcc75509487ed93
Joginder Singh <joginder.nitk@outlook.com>
2016-02-02 06:33:03 -0500

    Initial Setup of the project

---
+++ src/main/java/org/rey/springboot/Application.java
@@ -0,0 +1,16 @@
+package org.rey.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot Main application
+ * @author Reyansh
+ *
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String [ ] arg){
+		SpringApplication.run(Application.class,arg);
+	}
+}
